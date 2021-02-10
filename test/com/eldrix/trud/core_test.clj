ce1500647a308c23bcf49d3b3ae429d1f882501e
Mark Wardle <mark@wardle.org>
2021-02-10 21:07:49 +0000

    Fix errant newlines in circle CI configuration.
    Injecting secret creates an api-key file with a newline. Remove in code, but we'll also fix in config.

--- test/com/eldrix/trud/core_test.clj
+++ test/com/eldrix/trud/core_test.clj
@@ -1,10 +1,11 @@
 (ns com.eldrix.trud.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
             [com.eldrix.trud.release :as release]))
 
 
 (deftest get-releases
-  (if-let [api-key (slurp "api-key.txt")]
+  (if-let [api-key (str/trim-newline  (slurp "api-key.txt"))]
     (let [r58 (release/get-latest api-key 58)
           r341 (release/get-latest api-key 341)]
       (is r58)
